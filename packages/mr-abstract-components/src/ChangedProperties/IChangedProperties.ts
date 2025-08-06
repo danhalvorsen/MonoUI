18d696b61419f2500e873449ee9cb0ef87775cfb
Dan Edvard Halvorsen <danhalvorsen@users.noreply.github.com>
2025-08-06 23:13:36 +0200

    making it work

--- packages/mr-abstract-components/src/ChangedProperties/IChangedProperties.ts
+++
@@ -1,7 +0,0 @@
-export interface IChangedProperties {
-    hasChanged(key: string): boolean;
-    get<T = any>(key: string): T | undefined;
-    set<T = any>(key: string, value: T): void;
-    keys(): string[];
-  }
-  
